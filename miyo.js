3fff84ac6cbff83d5615d84ba4360f57aec99e59
Miyosora <wangkte1@yahoo.com.tw>
2018-06-06 18:50:24 +0800

    re

--- miyo.js
+++ miyo.js
@@ -1,15 +1,51 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 const http = require('http').Server(app);
-const sql = require('./sql.js')
 const port = Number(process.env.PORT || 80);
-const d=new Date();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const multers = multer();
 
+
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + "/" + month + "/" + day + " " + hour + ":" + min + ":" + sec;
+
+}
+
+const config ={
+    host: 'miyu.mysql.database.azure.com',
+    user: 'wangkte1@miyu',
+    password: 'Miyosora0',
+    database: 'miyo',
+    port: 3306,
+    ssl: true
+};
 
 http.listen(port, function() {
-  console.log("Listening on " + port+' '+d);
+  console.log("Listening on " + port+' '+getDateTime());
 });
-
+    app.use(bodyParser.urlencoded({extended:true}));
     app.use('/',express.static('public'));
     app.use('/public',express.static('public'));
             
@@ -32,9 +68,10 @@ http.listen(port, function() {
             res.sendfile('index.html');
         });
         app.get('/blog.html', function(req, res, next){
+            
             res.sendfile('blog.html');
+            
         });
-
         app.get('/model.html', function(req, res, next){
             res.sendfile('model.html');
         });
@@ -47,10 +84,89 @@ http.listen(port, function() {
         
         
         
-        
-        
-        
-        
+
+
+
+
+        app.post('/blog.html',function (req, res, next) {
+
+            console.log(req.body.username);
+            console.log(req.body.password);
+            console.log(req.body.txt);
+
+            const conn = new mysql.createConnection(config);
+                    conn.connect(
+                            function (err) { 
+                            if (err) { 
+                                console.log("!!! Cannot connect !!! Error:");
+                                throw err;
+                            }
+                            else
+                            {
+
+                    console.log("Connection established.");
+                    conn.query("select * from username where username ='"+req.body.username+"' and password ='"+req.body.password+"'",function (err, results)
+                    {
+                    if(results==""){
+                        console.log(results);
+                        res.sendfile('blog.html');
+                        console.log("end");
+                            conn.end(function(err) {
+                            
+                            });
+
+                    }
+                    else
+                        {
+                            
+                        if (err) throw err; 
+                            console.log("查詢成功"+process.uptime());  
+
+                            if(results[0].id>=1)
+                            
+                            {
+                            
+                            conn.query("INSERT INTO blog (time ,txt ) VALUES ('"+getDateTime()+"','"+req.body.txt+"');");
+                            conn.end(function(err) {
+                            
+                            });
+                            console.log("end");
+                           }
+
+                        
+                       }
+                   });
+                    
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+
+                   
+            }
+            
+        }
+        );
+
+});
+
+
+
         
         
         
--- sql.js
+++ sql.js
@@ -1,52 +1,8 @@
-var mysql = require('mysql');
 
-const config ={
-    host: 'miyu.mysql.database.azure.com',
-    user: 'wangkte1@miyu',
-    password: 'Miyosora0',
-    database: 'miyo',
-    port: 3306,
-    ssl: true
-};
-const conn = new mysql.createConnection(config);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
 
-conn.connect(
-    function (err) { 
-    if (err) { 
-        console.log("!!! Cannot connect !!! Error:");
-        throw err;
-    }
-    else
-    {
-       console.log("Connection established.");
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-         queryDatabase();
-    }   
-});
 
-function queryDatabase(){
-conn.end();
-console.log("end");
-};
+
+
