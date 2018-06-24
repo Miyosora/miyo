43560a1c5c14110ce3ed72d8325bb35524f132a9
Miyosora <wangkte1@yahoo.com.tw>
2018-06-25 01:26:49 +0800

    re

--- miyo.js
+++ miyo.js
@@ -12,8 +12,8 @@ function getDateTime() {
 
     var date = new Date();
 
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour+8;
+    var hour = date.getHours() +8; 
+    hour = (hour < 10 ? "0" : "") + hour;
 
     var min  = date.getMinutes();
     min = (min < 10 ? "0" : "") + min;
